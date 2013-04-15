4ea7bb9562c76478fbb9ae305b37f5863fdce309
Piers Mainwaring <piers@impossibly.org>
2013-04-15 11:37:30 -0600

    Added #action_assets reference in controllers.

--- lib/tension/controller.rb
+++ lib/tension/controller.rb
@@ -4,6 +4,13 @@ module Tension
   module Controller
     extend ActiveSupport::Concern
 
+    def action_assets
+      controller = request.symbolized_path_parameters[:controller]
+      action     = request.symbolized_path_parameters[:action]
+
+      Tension::Environment.find( controller, action )
+    end
+
     def assets
       Tension::Environment
     end
