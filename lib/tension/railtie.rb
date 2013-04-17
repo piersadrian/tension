e9c8d0077c535eded79def5f30c6c05934d5849d
Piers Mainwaring <piers@impossibly.org>
2013-04-17 15:04:27 -0600

    Fixed railtie in environments with 'cache_classes' enabled.

--- lib/tension/railtie.rb
+++ lib/tension/railtie.rb
@@ -4,20 +4,15 @@ module Tension
   require 'rails'
 
   class Railtie < Rails::Railtie
+
     initializer "tension.add_assets_to_precompile_list" do |app|
-
-      if app.config.cache_classes
-        app.reload_routes!
-        Tension::Environment.eager_load!
-      end
-
       ActionView::Base.send(:include, Tension::Helper)
       ActionController::Base.send(:include, Tension::Controller)
 
       Rails.application.config.assets.precompile << lambda do |path, filename|
         Tension::Environment.precompilation_needed?(path)
       end
-
     end
+    
   end
 end
