04ddbc811a2d885a5039b1b1847e2f0af64c0f24
Piers Mainwaring <piers@impossibly.org>
2013-05-03 14:04:11 -0600

    Fixed a missing class definition.

--- lib/tension/environment.rb
+++ lib/tension/environment.rb
@@ -1,108 +1,76 @@
 module Tension
-  module Utils
+
+  # This class describes routes and controller contexts, and through those contexts
+  # the assets to be included in templates.
+  #
+  class Environment
     class << self
 
-      SHARED_SUFFIX   = "_common".freeze
-      SHARED_REGEX    = /#{SHARED_SUFFIX}\..*\z/.freeze
-      EXTENSION_REGEX = /(#{SHARED_SUFFIX})?\..*\z/.freeze
-
-      # Matches strings like "blog/comments".
-      CONTROLLER_REGEX = /([\w\/]+[^\.])/.freeze
-
-      # Attempts to build a valid controller path from the given path String.
-      #   ARGS: path: a String path like "admin/blog_common.css"
+      # Loads a Context for the specified controller path.
       #
-      def controller_path(path)
-        if asset_path?(path)
-          return controller_for_asset_path(path)
+      def find(key)
+        fetch( Tension::Utils.controller_path(key) )
+      end
+      alias_method :[], :find
 
-        elsif matches = path.split("#").first.match(CONTROLLER_REGEX)
-          return matches[0]
+      # A Hash mapping controller paths to Contexts.
+      #
+      def contexts
+        @contexts ||= Hash.new
+      end
 
-        else
-          return nil
+      # Preloads all Contexts. Useful in environments where assets and controller
+      # actions don't change without an app reboot (production, staging).
+      #
+      def eager_load!
+        configured_get_defaults.each do |default|
+          find(default[:controller])
+        end
+
+        true
+      end
+
+      # Determines whether or not a given path refers to an asset that requires
+      # precompilation.
+      #
+      def precompilation_needed?(path)
+        if cxt = find(path)
+          Tension::Utils.shared_asset?(path) || cxt.has_action?( Tension::Utils.action_name(path) )
         end
       end
 
-      # Attempts to find an action name for a given asset path.
-      #
-      def action_name(path)
-        strip_file_extension( path.split("/").last ) if asset_path?(path)
-      end
-
-      # Determines whether an asset path is shared amongst all actions
-      # for its controller.
-      #
-      def shared_asset?(path)
-        !!path.match(SHARED_SUFFIX)
-      end
-
-      # Attempts to load an Asset from the Sprockets index. Uses the given
-      # Hash argument to build an asset path.
-      #   ARGS: options: a Hash containing e.g.
-      #     controller: "blog/comments"
-      #     action: "index"
-      #     type: Tension::CSS
-      #
-      def find_asset(options)
-        assets[ logical_asset_path(options) ]
-      end
-
-      # Returns the application-wide Sprockets Asset for the given type.
-      #   ARGS: type: Tension::JS or Tension::CSS
-      #
-      def global_asset(type)
-        assets[ "application.#{type}" ]
-      end
 
       private
 
-      # Builds a String path for an asset based on the given hash params.
-      #   ARGS: options: a Hash containing e.g.
-      #     controller: "blog/comments"
-      #     action: "index"
-      #     type: Tension::CSS
-      #
-      def logical_asset_path(options)
-        if options[:action].nil?
-          "#{options[:controller]}#{SHARED_SUFFIX}.#{options[:type]}"
-        else
-          "#{options[:controller]}/#{options[:action]}.#{options[:type]}"
+      def fetch(path)
+        contexts[path] || store(path) unless path.nil?
+      end
+
+      def store(path)
+        contexts[path] ||= Context.new(path) if valid_route?(path)
+      end
+
+      def valid_route?(controller)
+        configured_get_defaults.find do |default|
+          default[:controller] == controller
         end
       end
 
-      # Alias for Sprockets' asset index.
+      # Routing defaults (including controller path and action name) for all
+      # configured GET routes.
       #
-      def assets
-        Rails.application.assets
-      end
+      def configured_get_defaults
+        @configured_get_defaults ||= begin
+          get_routes = Rails.application.routes.routes.find_all do |route|
+            route.verb.match("GET")
+          end
 
-      # Takes an asset path like "comments/base/index.js" and determines
-      # the controller ("comments/base") that should serve that asset.
-      #   ARGS: path: a String path like "admin/blog_common.css"
-      #
-      def controller_for_asset_path(path)
-        parts = path.split("/")
+          @configured_get_defaults = get_routes.map do |route|
+            route.defaults unless route.defaults.empty?
+          end
 
-        if shared_asset?(parts.last)
-          parts.push(strip_file_extension( parts.pop ))
-        else
-          parts.pop
+          @configured_get_defaults.compact!
         end
-
-        return parts.any? ? parts.join("/") : nil
-      end
-
-      def strip_file_extension(path)
-        path.gsub(EXTENSION_REGEX, "")
-      end
-
-      # Returns whether or not the given path represents an asset file
-      # for which Tension is useful: JavaScript or CSS.
-      #   ARGS: path: a String path like "admin/blog_common.css"
-      #
-      def asset_path?(path)
-        path.match(/\.(#{ Tension::CSS }|#{ Tension::JS })\z/)
       end
 
     end
